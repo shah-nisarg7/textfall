06433a19552d215387f1f82b93b29684a9bac52e
shah-nisarg7 <nisargbshah111@gmail.com>
2026-07-21 23:13:35 +0530

    actually save typed text to storage

--- content.js
+++ content.js
@@ -13,8 +13,13 @@ document.addEventListener("input",function(event){
         return;      
         
     }
-
-    //logging in console for now we can see it working in devtools console
-    console.log("textfall detected typing", field.value);
-},true);
-                  
+    // using page url as key for now
+    //will change it later when fields are needed to be tracked seperately form same page
+    const key = window.location.href;
+    chrome.storage.local.set({[key]: field.value},function(){
+        //checking it if saves
+        console.log("textfall saved draft for",key);
+    });
+   
+},true);        
+                                    
